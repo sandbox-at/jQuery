3079ac3552714890a2dcc8b75ee03a9f1bcc86df
Bal Mukund <mbal@adobe.com>
2021-10-19 10:42:30 +0530

    initial commit

---
+++ removeElement.js
@@ -0,0 +1,6 @@
+// To remove elemenet , use .remove()
+$('h1').remove();
+
+// To remove element from one location and insert at another location, use .detach()
+//For html use https://musebeam.com/
+$("#collection-rows--collection-row-1").detach().insertBefore(".grid-uniform")
