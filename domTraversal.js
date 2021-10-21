608d874304c4eb0782b0aa4d8a60988c285e3b03
Bal Mukund <mbal@adobe.com>
2021-10-21 19:03:35 +0530

    initial commit

--- domTraversal.js
+++ domTraversal.js
@@ -19,5 +19,31 @@ $("h3").text();
 //This wil return all the ext together i.e ShopPolicies
 
 //.filter() , Add programmatic criteria for element to be selected.
+//Below code selects search bar element and check if any user has entered any text or not . If not changes border to red.
+
 $(".search-bar").filter(function(i,element){
-    return $(element).val().length===0}).css("border","1px solid red");
+    return $(element).val().length === 0 }).css("border","1px solid red");
+
+    //or
+    $('h3').filter(function(i,element){
+        return $(element).text().includes('Privacy');
+    })
+
+    //.has() selects the element and narrow down based on argument passed
+    ('div').has('h3'); //THis selects all the div on page which contains h3 element
+
+    //.is() , used to test the element
+    $('button').click(function(){
+        if ($(this).is(':even'))
+        {console.log("i am even")}
+        else{
+            console.log("I am odd");
+        }
+    })
+
+    //get parent of first h3 element
+    $('h3:first').parent();
+
+     $('h3:first').siblings();
+
+     $('div').find('li')
