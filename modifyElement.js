a1c3f5d45723f270edfdc2a930158779535c6c81
Bal Mukund <mbal@adobe.com>
2021-10-21 10:07:56 +0530

    commit

--- modifyElement.js
+++ modifyElement.js
@@ -1,2 +1,20 @@
 // modify element css using .css('property','value');
 $('p').css('color','red');
+
+// Get element attribute 
+//Returns class of paragraph
+$('p').attr('class'); 
+$('div').attr('id');
+
+// Set or modify attributes
+//Modify class
+
+$('p').attr('class','mukund');
+//remove Attribute
+// remove class attribute
+$('p').removeAttr('class');
+
+$('.h1 section-header--left').css('font-size','40px')
+
+
+
