50c9da9e1093f10d45615a7651f840285c3ee693
Bal Mukund <mbal@adobe.com>
2021-10-21 20:32:58 +0530

    initial commit

--- eventObject.js
+++ eventObject.js
@@ -1,9 +1,16 @@
 //e.target. events target property contains element that a given even is happening to.
 //E.g: If talking about click event , target will be the element click is happening to. Best usecase: get the current value of text input when user is typing
-
+$("p").click(function(event){
+    console.log(event);
+})
 
 
 //e.pageX and epageY , where on the page given event occured
 
 
-//e.timeStamp , when event is occured on the page
+//e.timeStamp , when event is occured on the page. Gives unix timestamp of when event is occured.
+
+
+//e.which , which is possible key is responsible for triggering the events(from key events)
+
+//e.stopPropogation , prevent the event from bubbling up to the surrounding elements.
